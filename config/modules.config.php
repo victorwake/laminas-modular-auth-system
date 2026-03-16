fb50aaa1eef95d307066156c82486100e455b9db
victorPinto <victor.pinto@ecix.tech>
2026-03-16 19:45:33 -0300

    first commit

---
+++ config/modules.config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Session',
+    'Laminas\Mvc\I18n',
+    'Laminas\I18n',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Application',
+    'Core',
+    'Auth',
+    'Shared',
+    'Etime',
+    'Eprivacy',
+];
